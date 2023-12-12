27eb20aec9bef5d0e160462da63a282588ad72e2
ksvvr <ksvvreddy@gmail.com>
2023-12-12 22:43:49 +0530

    level 6 before add tests

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
 
-app.get("/", (request,response) => {
-    response.send("hello world")
+const { Todo } = require("./models")
+
+app.get("/todos", (request,response) => {
+    console.log("Todo list")
+})
+
+app.post("/todos", async (request,response)=>{
+    console.log("Creating a todo", request.body)
+    try{
+        const todo = await Todo.addTodo({title:request.body.title,dueDate:request.body.dueDate})
+        return response.json(todo)
+    } catch (error) {
+        console.log(error)
+        return response.status(422).json(error)
+    }
+    
+})
+
+app.put("/todos/:id/markAsCompleted", async (request,response)=>{
+    console.log("We have to update a todo with ID:",request.params.id)
+    const todo = await Todo.findByPk(request.params.id)
+    try {
+        const updatedTodo = await todo.markAsCompleted()
+        return response.json(updatedTodo)
+    } catch (error) {
+        console.log(error)
+        return response.status(422).json(error)
+    }
+})
+
+app.delete("/todos/:id",(request,response)=>{
+    console.log("Delete a todo by ID: ",request.params.id)
 })
 
 app.listen(3000, () => {
